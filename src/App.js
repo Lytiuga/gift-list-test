b6ffa0d15f0ba6d9019db9af57c9a2c54e4fc066
Lytiuga <lityuga@gmail.com>
2018-07-31 19:26:58 -0600

    constructor props added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
+import data from "./Dataset.json";
+import { Container,Grid, Card, Icon, Button, Image } from "semantic-ui-react";
+
+
+
+
 
 class App extends Component {
+  constructor(props) {
+     super(props);
+
+     // This binding is necessary to make `this` work in the callback
+     this.buttClick = this.buttClick.bind(this);
+   }
+
+
+  buttClick = (adress) => {
+    console.log("Butt was clicked");
+
+    alert(
+      "Butt was clicked" + adress);
+    window.open("http://www.crea.com.ua", "_blank");
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <Grid columns={3} divided>
+            <Grid.Row>
+              {data.map(function(gifts) {
+                return (
+                  <Grid.Column key={gifts.id}>
+                    <Card>
+                      <Image src={gifts.img_src} />
+                      <Card.Content>
+                        <Card.Header>{gifts.person}</Card.Header>
+                        <Card.Meta>
+                          <span>
+                            Dreaming about {gifts.gift}, $ {gifts.price} cost
+                          </span>
+                        </Card.Meta>
+                      </Card.Content>
+                      <Card.Content extra>
+
+                      { /*<Button adress={gifts.giftlink}> Go to {gifts.person}'s Gitf</Button> */}
+
+                       <Button onClick={function() {this.buttClick(gifts.giftlink)}}> Go to {gifts.person}'s Gitf</Button>
+
+
+
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                );
+              })}
+
+            </Grid.Row>
+            <Grid.Row>
+            <Button onClick={this.buttClick} adress="www.google.com">
+              Working Button
+            </Button>
+            </Grid.Row>
+          </Grid>
+        </Container>
       </div>
     );
   }
